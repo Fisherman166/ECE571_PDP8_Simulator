1d9926f080d28b57e6cb35991b103ed50c4016fc
Fisherman166 <koppen2@eve.ece.pdx.edu>
2015-10-22 17:57:25 -0700

    Algorithimic test that writes all locations and then reads all locations

--- memory_controller_tb.sv
+++ memory_controller_tb.sv
@@ -19,23 +19,66 @@ module memory_controller_tb();
         .read_data(read_data)
     );
 
+    typedef struct packed {
+        word address, data;
+    } inputs_container;
+
+    inputs_container inputs;
+
     always @(read_data) begin
-        $display("Read_data = %04o", read_data);
+        if(inputs.data !== read_data) begin
+            $display("Expected to read = %04o from address %04o, actually read =%04o",
+                     inputs.data, inputs.address, read_data);
+        end
     end
+
+    task apply_stimulus(input word address_to_write, input word data_to_write);
+        address = address_to_write;
+        write_data = data_to_write;
+        write_enable = 1'b1;
+        #1;
+        write_enable = 1'b0;
+        #1;
+    endtask
+
+    task read_from_memory(input word address_to_read);
+        address = address_to_read;
+        read_type = DATA_READ;
+        read_enable = 1'b1;
+        #1;
+        read_enable = 1'b0;
+        #1;
+    endtask
+
+    task write_all_addresses;
+        automatic word address_to_write = 12'o0;
+        automatic word data_to_write = 12'o0;
+
+        for(int i = 0; i < 12'o7777; i++) begin
+            inputs.address = address_to_write;
+            inputs.data = data_to_write;
+            apply_stimulus(address_to_write, data_to_write);
+            read_from_memory(address_to_write);
+            address_to_write += 1'b1;
+            data_to_write += 1'b1;
+        end
+    endtask
 
     initial begin
         init_mem();
         trace_init();        
-
-        address = 12'o 200;
-        write_data = 12'o 133;
-        write_enable = 1;
+        read_enable = 1'b0;
+        write_enable = 1'b0;
         #1;
 
-        write_enable = 0;
-        read_type = DATA_READ;
-        read_enable = 1'b1;
-        #1;
+        //Directed test just to write then read
+        inputs.address = 12'o200;
+        inputs.data = 12'o333;
+        apply_stimulus(inputs.address, inputs.data);
+        read_from_memory(inputs.address);
+
+        //Algorithimic test
+        write_all_addresses();
 
         print_valid_memory();
         trace_close();
