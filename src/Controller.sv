9e2460e80aa71b85c19374523a09f9de98d7f7ae
Jon Waldrip <jwaldrip@pdx.edu>
2015-11-24 21:49:55 -0800

    Update

--- src/Controller.sv
+++ src/Controller.sv
@@ -351,16 +351,14 @@ always_comb begin: Output_Logic
                          bus.LK_ctrl = LK_MICRO;
                     end     
      
-          MIC_2:    if ({bus.micro_g2,bus.skip} == 2'b11) begin
-                         bus.PC_ctrl = PC_P1;
-                         if ({bus.micro_g2,bus.curr_reg.ir[7:2]} == 7'b1??????) begin
-                         bus.AC_ctrl = AC_CLEAR;
-                         end
-                    end     
+          MIC_2:    if ({bus.micro_g2,bus.skip} == 2'b11)
+                         bus.PC_ctrl = PC_P1; 
                     else if ({bus.micro_g2,bus.curr_reg.ir[7:2]} == 7'b11????1)
                          bus.AC_ctrl = AC_SWREG;
                     else if ({bus.micro_g2,bus.curr_reg.ir[2]} == 2'b11)
                          bus.AC_ctrl = AC_OR_SR;
+                    else if ({bus.micro_g2,bus.curr_reg.ir[7:2]} == 7'b1??????)
+                         bus.AC_ctrl = AC_CLEAR;
           MIC_3:    if ({bus.micro_g3,bus.curr_reg.ir[7]} == 2'b11) 
                          bus.AC_ctrl = AC_CLEAR;
           MIC_4:    if ({bus.micro_g3,bus.curr_reg.ir[6:4]} == 4'b11?1) begin
